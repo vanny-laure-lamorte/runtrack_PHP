c18a757108692e05491608fde623e41bc016f61d
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-23 17:43:41 +0200

    (feat) add exo 02

---
+++ job02/index.php
@@ -0,0 +1,14 @@
+<?php
+$phrase = "Hello all! <br>";
+
+// Commentaire type 1
+echo $phrase; 
+
+# Commentaire type 2 
+echo $phrase; 
+
+/* Commentaire 
+type 3: sur plusieurs lignes */
+echo $phrase;
+
+?>
