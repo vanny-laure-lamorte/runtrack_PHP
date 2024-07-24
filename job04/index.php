ebd596d5c328e6fd8c60e38a4d468d728af1548f
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-24 11:23:33 +0200

    (feat) job04

---
+++ job04/index.php
@@ -0,0 +1,26 @@
+
+<!-- Affichez tous les nombres de 0 à 1337 compris sauf 26, 37, 88, 1111 en mettant
+un retour à la ligne entre chaque nombre -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+for ($i = 0; $i<1337; $i++){
+    if ($i==26|| $i == 37 || $i == 88 || $i ==1111){
+        continue;
+    }
+    else {
+        echo "$i <br>";
+    }
+}
+?>
+
+</body>
+</html>
