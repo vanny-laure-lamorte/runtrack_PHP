72ae35f067353d43641780ec08086089b293c23d
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-24 12:13:58 +0200

    (feat) job07

---
+++ job07/index.php
@@ -0,0 +1,12 @@
+<!-- Créez une fonction nommée “hello()”. Cette fonction doit afficher “Hello La
+Plateforme !” -->
+
+<?php
+
+function hello(){
+    echo "Hello La Plateforme !";
+}
+
+hello();
+
+?>
